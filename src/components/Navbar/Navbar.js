5d5216a807431dc0eb6f6e2e687bdb6f6de09630
Arzu Caner <arzuguneycaner@gmail.com>
2025-04-28 23:04:14 +0100

    Fix mobile navigation layout and improve responsive menu UX

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md"; 
+import { MdFingerprint } from "react-icons/md";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
@@ -9,9 +9,16 @@ function Navbar() {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [isMenuOpen]);
 
   return (
-    <nav className={styles.navbar}>    
+    <nav className={styles.navbar}>
       <div className={styles.logoContainer}>
         <Link to="/" className="animated-icon">
           <MdFingerprint size={40} />
@@ -19,20 +26,48 @@ function Navbar() {
         <h1 className={styles.logo}>CodeArz</h1>
       </div>
 
-        <div className={styles.hamburger} onClick={toggleMenu}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
-    
+
       <ul className={`${styles.navLinks} ${isMenuOpen ? styles.showMenu : ""}`}>
-        <li><Link to="/">Baker Street</Link></li>
-        <li><Link to="/about">Case Files</Link></li>
-        <li><Link to="/blog">Journal</Link></li>
-        <li><Link to="/projects">Solved Cases</Link></li>
-        <li><Link to="/opensource">Clues Shared</Link></li>
-        <li><Link to="/products">Inventor's Lab</Link></li>
-        <li><Link to="/speaking">Public Cases</Link></li>
+        <li>
+          <Link to="/" onClick={() => setIsMenuOpen(false)}>
+            Baker Street
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={() => setIsMenuOpen(false)}>
+            Case Files
+          </Link>
+        </li>
+        <li>
+          <Link to="/blog" onClick={() => setIsMenuOpen(false)}>
+            Journal
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects" onClick={() => setIsMenuOpen(false)}>
+            Solved Cases
+          </Link>
+        </li>
+        <li>
+          <Link to="/opensource" onClick={() => setIsMenuOpen(false)}>
+            Clues Shared
+          </Link>
+        </li>
+        <li>
+          <Link to="/products" onClick={() => setIsMenuOpen(false)}>
+            Inventor's Lab
+          </Link>
+        </li>
+        <li>
+          <Link to="/speaking" onClick={() => setIsMenuOpen(false)}>
+            Public Cases
+          </Link>
+        </li>
       </ul>
     </nav>
   );
