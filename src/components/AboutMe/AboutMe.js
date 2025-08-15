08787fe0d76025977e3e73fcae3e86ec1ba2fdd8
Arzu Caner <arzuguneycaner@gmail.com>
2025-08-15 21:30:16 +0100

    Add new notes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -20,7 +20,8 @@ function AboutMe() {
     "2024: Spoke on International Women’s Day about empowering women in technology.",
     "2024: Launched a knitting app (KnittMates) to promote mental health post-COVID.",
     "2025: Received the AI Visionary Award from the Frontend Queens Community.",
-    "2025: Participated in AI Safety Hackathon and created Safeai Academy."
+    "2025: Participated in AI Safety Hackathon and created Safeai Academy.",
+    "2025: Participated in and created Mind Garden: AI-Powered Multi-Agent.(with amazing team)",    
   ];
 
   return (
